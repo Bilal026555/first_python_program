bb3f338d2ecf7ed514af43ac98ca060f0e9fe26c
Bilal Uddin <34515992+Bilal026555@users.noreply.github.com>
2020-08-19 22:16:33 -0230

    Update inverted_peramid_with_fixed_num.py

--- inverted_peramid_with_fixed_num.py
+++ inverted_peramid_with_fixed_num.py
@@ -6,3 +6,10 @@ for i in range(n,0,-1):
    for j in range (1,i+1):
       print(b,end=" ")
    print("")
+b=n
+for i in range(n,0,-1):
+   for j in range (1,i+1):
+      print(b,end=" ")
+   print("")
+   b=b-1
+   
