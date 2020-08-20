7cf5feb2c39d939cf7b384a237712f466567719f
Bilal Uddin <34515992+Bilal026555@users.noreply.github.com>
2020-08-19 22:46:30 -0230

    Create pattern_square.py

---
+++ pattern_square.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a positive integer greater than orequal to 3"))
+for i in range(1, n+1):
+   # num = 1
+   a="*"
+   b=" "
+   for j in range(n, 0, -1):
+      print(i, end=' ')
+   print("")
+       
